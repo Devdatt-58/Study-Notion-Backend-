fcc2abdb6d82b226b3888983797b2f78d79b8804
Devdatt-58 <singhdevdatt828@gmail.com>
2026-01-01 23:44:58 +0530

    Add database configuration

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+exports.connectWithDb = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URL);
+        console.log("Database connected successfully");
+    } catch (error) {
+        console.error("Database connection failed");
+        console.error(error.message);
+        process.exit(1);
+    }
+};
